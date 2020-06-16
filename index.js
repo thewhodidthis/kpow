b2b611a8ce754d6abd91bc59775da3029e155278
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-06-16 16:49:39 +0100

    module: clean up

--- index.js
+++ index.js
@@ -6,65 +6,76 @@ const path = require('path')
 const { createServer } = require('http')
 const { exec } = require('child_process')
 
-const html = fs.readFileSync(path.resolve(__dirname, 'index.html'))
-const file = process.argv[2]
+const fileInput = process.argv[2]
 const port = process.env.PORT || 1999
 
-const boot = (seed) => {
+if (fileInput) {
+  load(fs.readFileSync(fileInput))
+} else {
+  const result = []
+
+  process.stdin.resume()
+  process.stdin
+    .on('error', console.error)
+    .on('data', (chunk) => {
+      result.push(chunk)
+    })
+    .on('end', () => {
+      load(Buffer.concat(result))
+    })
+}
+
+const hostPath = path.resolve(__dirname, 'index.html')
+const host = fs.readFileSync(hostPath)
+
+function load(seed) {
+  // Keeps track of files loaded
   const ledger = []
 
+  // Loads up to two files HTML and JS before shutting down
   const server = createServer(({ url }, res) => {
-    const isjs = url.includes('.js')
+    // Figure out content type by looking at file extension
+    let content = host
+    let contentType = 'html'
 
-    const data = isjs ? seed : html
-    const type = isjs ? 'javascript' : 'html'
-    const size = Buffer.byteLength(data)
+    if (url.includes('.js')) {
+      content = seed
+      contentType = 'javascript'
+    }
 
     res.writeHead(200, {
       'Connection': 'close',
-      'Content-Length': size,
-      'Content-Type': `text/${type}`
+      'Content-Length': Buffer.byteLength(content),
+      'Content-Type': `text/${contentType}`
     })
 
-    // Save url, quit once corresponding HTML template and input JS have been served
-    res.end(data, () => {
+    // Save url just passed in, quit once corresponding
+    // HTML host and input JS have been served
+    res.end(content, () => {
       ledger.push(url)
 
       if (ledger.length >= 2) {
         server.close()
       }
     })
-  }).listen(port, () => {
-    let cmd = 'xdg-open'
+  })
+
+  // Start listening, attempt to open host HTML in a browser window based on OS
+  server.listen(port, () => {
+    let command = 'xdg-open'
 
     if (process.platform === 'win32') {
-      cmd = 'start'
+      command = 'start'
     }
 
     if (process.platform === 'darwin') {
-      cmd = 'open'
+      command = 'open'
     }
 
-    exec(`${cmd} http://localhost:${port}/index.html`, (error) => {
+    exec(`${command} http://localhost:${port}/index.html`, (error) => {
       if (error) {
         console.error(error.message)
       }
     })
   })
-}
-
-if (file) {
-  boot(fs.readFileSync(file))
-} else {
-  const body = []
-
-  process.stdin.resume()
-  process.stdin
-    .on('error', console.error)
-    .on('data', (chunk) => {
-      body.push(chunk)
-    })
-    .on('end', () => {
-      boot(Buffer.concat(body))
-    })
 }
